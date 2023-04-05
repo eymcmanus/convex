3b7cfe904be1534eb4227aaa5d781ca8d2de0ed8
Yumeng Bai <you@example.com>
2023-04-05 08:59:12 -0400

    added drop box for num people

--- convex/showReservation.js
+++ convex/showReservation.js
@@ -1,5 +1,8 @@
 import { query } from "./_generated/server";
 
-export default query(async ({ db }) => {
-  return await db.query("avail_reservations").collect();
+export default query(async ({ db }, {num_people}) => {
+  if (num_people === undefined) {
+    num_people =0
+  }
+  return await db.query("avail_reservations").filter(q => q.gte(q.field("max_people"), num_people)).collect();
 });
--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { useMutation, useQuery } from "../convex/_generated/react";
 
 export default function App() {
-  const reservations = useQuery("showReservation") || [];
+ 
   const [newMessageText, setNewMessageText] = useState("");
+  const [num_people, set_num_people]= useState("");
   const sendMessage = useMutation("sendMessage");
   const makeReservation = useMutation("makeReservation");
 
@@ -14,17 +15,39 @@ export default function App() {
     await sendMessage({ body: newMessageText, author: name });
   }
 
+
   async function handleMakeReservation(id, name, restaurant_name, date, time) {
-    console.log(id);
     await makeReservation({ id: id, name: name, restaurant_name: restaurant_name, date: date, time: time});
   }
-
+  const reservations = useQuery("showReservation", num_people||0) || [];
+  const [open, setOpen] = useState(false);
   return (
     <main>
       <h1>Restaurant reservation</h1>
       <p className="badge">
         <span>{name}</span>
       </p>
+
+      <div className="dropdown">
+      <button onClick={() => setOpen(!open)}>Dropdown</button>
+      {open ? (
+        <ul className="menu">
+          <li className="menu-item">
+            <button>1</button>
+          </li>
+          <li className="menu-item">
+            <button>2</button>
+          </li>
+          <li className="menu-item">
+            <button>3</button>
+          </li>
+          <li className="menu-item">
+            <button>4</button>
+          </li>
+        </ul>
+      ) : null}
+      {open ? <div>Is Open</div> : <div>Is Closed</div>}
+    </div>
 
       <span>restaurant_name</span>
       <span>date</span>
